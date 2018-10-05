dfe96b5a91791ab18002eaac73289e573630dce0
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-10-06 02:25:20 +0530

    Sending Nested objects to the dummy component

--- src/component/dummy/dummy.tsx
+++ src/component/dummy/dummy.tsx
@@ -1,6 +1,15 @@
 import * as React from "react";
-export class Dummy extends React.Component<{},{}>{
+export class Dummy extends React.Component<NamedParam, {}>{
+
     render(){
-        return <h1> Hello World </h1>;
+        return <h1> Hello World {this.props.address.country} {this.props.address.state}</h1>;
     }
+}
+
+interface NamedParam {
+    address: Address;
+}
+interface Address {
+    country: string,
+    state: string
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {Dummy} from "./component/dummy/dummy";
-
+const addressV = {country: "India", state: "Jharkhand"};
 const ROOT = document.querySelector('.root');
-ReactDOM.render(<Dummy></Dummy>, ROOT);
+ReactDOM.render(<Dummy address={{country: "India", state:"Jharkhand"}}/>, ROOT);

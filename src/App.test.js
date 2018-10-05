b91949bb97d383282998f5ccc2af530457dc6c30
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-10-05 23:40:29 +0530

    Added first component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<AuthorQuiz />, div);
   ReactDOM.unmountComponentAtNode(div);
 });

6518450258b4789711d2d248cb16fe685891cb06
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-10-06 14:59:10 +0530

    Another version of conditional render.

--- src/component/login/login.tsx
+++ src/component/login/login.tsx
@@ -8,9 +8,14 @@ export class Login extends React.Component<any, LoginState>{
 
     render(){
         let isLoggedIn = this.state.isLoggedIn;
+        let element ;
+        if(isLoggedIn){
+            element = <button onClick={this.logout}>Log-out</button>
+        }else{
+            element = <button onClick={this.login}> Log-In</button>
+        }
         return <div>
-            The user is <b>{isLoggedIn ? 'Logged In' : 'not logged in'}</b> <button onClick={isLoggedIn ? this.logout :
-            this.login}>{isLoggedIn ? 'Logout': 'Login'}</button>
+            The user is <b>{isLoggedIn ? 'Logged In' : 'not logged in'}</b> {element}
         </div>
     }
 
